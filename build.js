426f21df03e468a0d79b8e8f45ca05152f366088
websanova <rob@websanova.com>
2018-03-20 16:08:16 +0700

    Update to new structure.

--- build.js
+++ build.js
@@ -1,38 +1,45 @@
 var fs = require('fs');
 
-function readFiles(dirname, onFileContent, onError) {
-  fs.readdir(dirname, function(err, filenames) {
+function readFiles(dirname, sub, onFileContent, onError) {
+  fs.readdir(dirname + '/' + sub, function(err, filenames) {
     if (err) {
       onError(err);
       return;
     }
+    
     filenames.forEach(function(filename) {
-      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+      fs.readFile(dirname + '/' + sub + '/' + filename, 'utf-8', function(err, content) {
+        
+        console.log(dirname + '/' + sub + '/' + filename);
+        
         if (err) {
-          onError(err);
-          return;
+          // onError(err);
+          // return;
         }
-        onFileContent(filename, content);
+        else {
+          onFileContent(sub, filename, content);
+        }
       });
     });
   });
 }
 
-readFiles('svg/', function(filename, content) {
+function processFile(sub, filename, content) {
 
     file = filename.split('.');
 
     if (file[1] === 'svg') {
         content = 'module.exports = {template:\'<div class="ico">' + content + '</div>\'};';
 
-        fs.writeFileSync('./js/' + file[0] + '.js', content, {
+        fs.writeFileSync('./js/' + sub + '/' + file[0] + '.js', content, {
             encoding:'utf8',
             flag:'w'
         });
 
-        fs.unlink('./svg/' + filename);
+        // fs.unlink('./svg/' + sub + '/' + filename);
     }
+}
 
-}, function(err) {
-  throw err;
-});
+readFiles('svg/', 'brands', processFile, function(err) {throw err;});
+readFiles('svg/', 'regular', processFile, function(err) {throw err;});
+readFiles('svg/', 'solid', processFile, function(err) {throw err;});
